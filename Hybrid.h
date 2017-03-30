1a4d63529334f647b2fa3689edd943782ea32e75
Jesse Robert Finley <jrfinley42@students.tntech.edu>
2017-03-30 09:50:54 -0500

    FIXED SPACINGgit add *.h!

--- Hybrid.h
+++ Hybrid.h
@@ -9,33 +9,33 @@ template < class T >
 class Hybrid
 {
 
-   private:
-      QueueLinked<T>* q;
-      SortedListDoublyLinked<T>* sldl;
+	private:
+		QueueLinked<T>* q;
+		SortedListDoublyLinked<T>* sldl;
 
-   public:
-      Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item));
-      ~Hybrid();
+	public:
+		Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item));
+		~Hybrid();
 
-      bool isEmpty();
-      void enqueue(T* item);
-      T* dequeue();
-      ListDoublyLinkedIterator<T>* iterator();
+		bool isEmpty();
+		void enqueue(T* item);
+		T* dequeue();
+		ListDoublyLinkedIterator<T>* iterator();
 
 };
 
 template < class T >
 Hybrid<T>::Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item))
 {
-   q = new QueueLinked<T>();
-   sldl = new SortedListDoublyLinked<T>(comp_items, comp_keys);
+	q = new QueueLinked<T>();
+	sldl = new SortedListDoublyLinked<T>(comp_items, comp_keys);
 }
 
 template < class T >
 Hybrid<T>::~Hybrid()
 {
-   delete q;
-   delete sldl;
+	delete q;
+	delete sldl;
 }
 
 //DO THIS
